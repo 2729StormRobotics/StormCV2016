bfea194dbd638719c9934d05dd6afed4ba6c81a9
Ayernam <namhaza4@outlook.com>
2016-02-02 15:42:31 -0500

    Adding basic testing code

--- src/StormCV2016.java
+++ src/StormCV2016.java
@@ -1,9 +1,58 @@
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class StormCV2016 {
 
+    static int session;
+    static Image frame;
+    
+    public static NetworkTable table;
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		//start SmartDashboard
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("roboRIO-2729-FRC.local");
+		table = NetworkTable.getTable("SmartDashboard");
+		*/
+				
+		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 
+        // the camera name (ex "cam0") can be found through the roborio web interface
+        session = NIVision.IMAQdxOpenCamera("cam0",
+                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
+        
+        NIVision.IMAQdxStartAcquisition(session);
+
+        /**
+         * grab an image, draw the circle, and provide it for the camera server
+         * which will in turn send it to the dashboard.
+         */
+        NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+
+        while (true) {
+            NIVision.IMAQdxGrab(session, frame, 1);
+            NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+            
+            CameraServer.getInstance().setImage(frame);
+                        
+            detectTarget();
+        }
+        
+        //NIVision.IMAQdxStopAcquisition(session);
+	}
+
+	private static void detectTarget() {
+		
+		
 	}
 
 }
