f7de39be666cf7bac53bdd6968c7b3431d526bd1
Ayernam <namhaza4@outlook.com>
2016-02-03 17:18:57 -0500

    Creating IterativeRobot structure

---
+++ src/org/usfirst/frc/team2729/robot/Robot.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team2729.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+
+    public void robotInit() {
+    	
+    }
+
+    public void autonomousInit() {
+    	
+    }
+
+    public void autonomousPeriodic() {
+    	
+    }
+
+    public void teleopPeriodic() {
+        
+    }
+
+    public void testPeriodic() {
+    
+    }
+    
+}
